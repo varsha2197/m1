850daac79c7cef324f26fdb997e33fd7320cb097
Varsha Srinivasan <varsha2197@gmail.com>
2018-08-07 05:59:48 -0600

    Adding code outline and documentation for C1M1 Assessment

--- assessments/m1/stats.c
+++ assessments/m1/stats.c
@@ -8,15 +8,22 @@
  * misuse of this material. 
  *
  *****************************************************************************/
+
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats 
+ * @brief Analyzes and prints statistical data like mean, medium, maximum 
+ * and minimum number present in a char array, followed by sorting the 
+ * array in descending order.
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
- *
+ * This program analyzes an array of unsigned char data items and   
+ * reports analytics on the maximum, minimum, mean, and median of the 
+ * data set. In addition, the array is also reorderedin descnding order, 
+ * i.e., from large to small. All statistics are rounded down to the 
+ * nearest integer.
+ * 
+ * @author Varsha Srinivasan
+ * @date 3rd August 2018
+ * 
  */
 
 
@@ -25,19 +32,67 @@
 #include "stats.h"
 
 /* Size of the Data Set */
-#define SIZE (40)
+
+#define SIZE 40
+
+int ma,mi;
+int mea,med, pos1,pos2,pos3;
+
+void print_array(unsigned char *,  unsigned int);
+int find_maximum(unsigned char *, unsigned int);
+int find_minimum(unsigned char *, unsigned int);
+int find_mean(unsigned char *,  unsigned int);
+int find_median(unsigned char *,  unsigned int);
+void sort_array(unsigned char *, const unsigned int);
+void print_statistics();
+
 
 void main() {
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
-
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+	  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+		                                114, 88,   45,  76, 123,  87,  25,  23,
+						                              200, 122, 150, 90,   92,  87, 177, 244,
+									                                    201,   6,  12,  60,   8,   2,   5,  67,
+													                                    7,  87, 250, 230,  99,   3, 100,  90}; 
+	    
+	    print_array(test,SIZE);
+	      ma= find_maximum(test,SIZE);
+	        mi= find_minimum(test,SIZE); 
+		  mea= find_mean(test,SIZE);
+		    med= find_median(test,SIZE);
+		      print_statistics();
+		        sort_array(test,SIZE);
+			    
+			  /* Other Variable Declarations Go Here */
+			  /* Statistics and Printing Functions Go Here */
 
 }
 
-/* Add other Implementation File Code Here */
+void print_array(unsigned char *t, unsigned int size)
+{ 
+}
+
+int find_maximum(unsigned char *t,  unsigned int size)
+{
+}
+
+int find_minimum(unsigned char *t, unsigned int size)
+{ 
+}
+
+int find_mean(unsigned char *t,unsigned int size)
+{ 
+}
+
+int find_median(unsigned char *T, unsigned int size)
+{
+}
+
+void sort_array(unsigned char *T, unsigned int size)
+{
+}
+
+void print_statistics()
+{
+}
+
--- assessments/m1/stats.h
+++ assessments/m1/stats.h
@@ -8,33 +8,136 @@
  * misuse of this material. 
  *
  *****************************************************************************/
+
+
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
- *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
- *
+ * @file Analyzes and prints statistical data like mean, medium, maximum 
+ * and minimum number present in a char array, followed by sorting the 
+ * array in descending order.
+ *        
+ * This program analyzes an array of unsigned char data items and   
+ * reports analytics on the maximum, minimum, mean, and median of the 
+ * data set. In addition, the array is also reorderedin descnding order, 
+ * i.e., from large to small. All statistics are rounded down to the 
+ * nearest integer.
+ *               
+ * @author Varsha Srinivasan
+ * @date 7th August 2018
+ *                        
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
 
 /**
- * @brief <Add Brief Description of Function Here>
- *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * @brief Prints the array when Given an array of data and a length.
+ *     
+ * This function inputs an unsigned char array as a pointer and the length
+ * of the array and displays the array from left to right using a 'for'
+ * loop. 
+ *            
+ * @param *t the array to be displayed
+ * @param size the length of the array
+ * @return nothing to be returned
  */
+void print_array(unsigned char *,  unsigned int);
+
+
+/**
+ * @brief Given an array of data and a length, returns the maximum.
+ *    
+ * This function inputs an unsigned char array as a pointer and the length
+ * of the array and finds the maximum number by first assigning first 
+ * element to 'max'. It then compares every element with max and if an 
+ * element is larger than max then max is assigned the new element and so
+ * on. The function finally returns the maximum value.
+ *              
+ * @param *t the array to be displayed
+ * @param size the length of the array
+ * @return the maximum value present in the array.
+ */
+int find_maximum(unsigned char *, unsigned int);
+
+
+/**
+ * @brief Given an array of data and a length, returns the minimum.
+ *    
+ * This function inputs an unsigned char array as a pointer and the length
+ * of the array and finds the minimum number by first assigning first 
+ * element to 'min'. It then compares every element with min and if an 
+ * element is smaller than max, then min is assigned the new element.
+ * The function finally returns the minimum value.
+ *               
+ * @param *t the array to be displayed
+ * @param size the length of the array 
+ * @return the minimum value present in the array.
+ */
+int find_minimum(unsigned char *, unsigned int);
+
+
+/**
+ * @brief Given an array of data and a length, returns the mean of the  
+ * dataset.
+ *      
+ * This function inputs an unsigned char array as a pointer and the length
+ * of the array and calculates the sum of the elements and then dividing  
+ * the sum(total) by the size of the array.The function finally returns 
+ * the mean value.
+ *               
+ * @param *t the array to be displayed
+ * @param size the length of the array 
+ * @return the mean value of the array.
+ */
+int find_mean(unsigned char *,  unsigned int);
+
+
+/**
+ * @brief Given an array of data and a length, returns the median of the  
+ * dataset.
+ *     
+ * This function inputs an unsigned char array as a pointer and the length
+ * of the array and sorts array in ascending order.
+ * Median= (20th element + 21st element)/2.  
+ * The function finally returns the median value.
+ *               
+ * @param *T the array to be displayed
+ * @param size the length of the array 
+ * @return the median value of the array.
+ */
+int find_median(unsigned char *,  unsigned int);
+
+
+/**
+ * @brief Given an array of data and a length, sorts the array in 
+ * descending order.
+ *      
+ * This function inputs an unsigned char array as a pointer and the length
+ * of the array and uses selection sort algorithm to sort the array from 
+ * largest to smallest. It compares the first element with all other 
+ * elements and swaps it with the largest element. Thus, at the end of the
+ * first iteration the largest element will be present in the beginning of
+ * the array. Similarly after all the iterations are over, the array would
+ * have been successfully sorted in descending order.
+ *                     
+ * @param *T the array to be sorted
+ * @param size the length of the array
+ * @return nothing
+ */
+void sort_array(unsigned char *, const unsigned int);
+
+
+/**
+ * @brief prints the statistics of an array including minimum, maximum, 
+ * mean, and median
+ *       
+ * The function prints the statistics like maximum, minimum, mean and 
+ * median of the unsorted array. It uses globally declared variables
+ * and hence doesn't need any input.
+ *           
+ * @param none
+ * @return none
+ */
+void print_statistics();
 
 
 #endif /* __STATS_H__ */
